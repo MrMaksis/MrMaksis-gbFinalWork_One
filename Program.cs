7dca2414979db161be7391a66701df8a8ec2bce5
mr_maksis <max.morink@yandex.ru>
2023-02-13 21:08:24 +0300

    Add function FindResultArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FindResultArray(string[] arr)                  //Функция отбора массиива
+{
+    string[] outArr = new string[arr.Length];
+    int count = 0;
+
+    for(int i = 0; i < arr.Length; i++){
+        if(arr[i].Length <= 3){
+            outArr[count] =  arr[i];
+            count++;
+        }
+    }
+
+    //Записывает число количества непустых элементов
+    outArr[outArr.Length - 1] = count.ToString();                   
+    return outArr;
+} 
